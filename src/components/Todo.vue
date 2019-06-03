05f4d210d23c875778252261fd4d027f3bd52d3b
BaTiany <18296970472@163.com>
2019-06-03 23:21:53 +0800

    Vue/CLI 搭建 TodoList

---
+++ src/components/Todo.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="todo">
+    <input
+      v-model="inputVal"
+      type="text"
+      placeholder="请输入内容..."
+      />
+    <button @click="addTodo">Add</button>
+
+    <ul>
+      <TodoItem
+        v-for="(item,index) in todos"
+        :key="index"
+        :todo="item"
+        :index="index"
+        @abc="delTodo"
+       />
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+  data () {
+    return {
+      inputVal:'',
+      todos:[
+        '吃饭',
+        '睡觉',
+        '打豆豆'
+      ]
+    }
+  },
+
+  components: {
+    TodoItem
+  },
+
+  methods: {
+    addTodo () {
+      this.todos.push(this.inputVal);
+    },
+
+    delTodo (index) {
+      this.todos.splice(index,1)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
